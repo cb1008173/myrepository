46651daa408525018066c01e818d9d5303ab2460
connor baun <135280720+cb1008173@users.noreply.github.com>
2023-06-15 20:37:07 -0400

    Update setup_control.sh

--- setup_control.sh
+++ setup_control.sh
@@ -3,3 +3,12 @@
 #generate passswordless ssh
 runuser -u cb100817 -- ssh-keygen -q -t rsa -f /users/cb100817/.ssh/id_rsa -N ''
 runuser -u cb100817 -- cat /users/cb100817/.ssh/id_rsa.pub >> /users/cb100817/.ssh/authorized_keys
+
+#setup NFS for key sharing
+apt update
+apt install -y nfs-kernel-server
+mkdir /var/nfs/keys -p
+cp /users/cb100817/.ssh/id_rsa* /var/nfs/keys
+chown nobody:nogroup /var/nfs/keys
+echo "/var/nfs/keys 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+systemctl restart nfs-kernel-server
