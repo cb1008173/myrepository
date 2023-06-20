29b0d8bbe55c1f657161c68c00e5dfda772d1d22
connor baun <135280720+cb1008173@users.noreply.github.com>
2023-06-20 18:17:24 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -9,11 +9,13 @@ prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
 #Create a XenVM
-for i in range(2):
+for i in range(3):
   if i == 0:
-    node = request.XenVM("control")
+    node = request.XenVM("webserver")
+  elif i == 1:
+    node = request.XenVM("observer")
   else:
-    node = request.XenVM("host")
+    node = request.XenVM("ldap")
 
   node.routable_control_ip = "true"
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
@@ -22,10 +24,10 @@ for i in range(2):
   iface.addAddress(rspec.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
   
-  if i == 0:
-    node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_control.sh"))
-  else:
-    node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_host.sh"))
+  #if i == 0:
+  #  node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_control.sh"))
+  #else:
+   # node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_host.sh"))
   
   
 # Print the RSpec to the enclosing page.
