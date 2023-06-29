a243ad9b7e518a6b58256005847d4002f2bc7e9d
connor baun <135280720+cb1008173@users.noreply.github.com>
2023-06-29 12:49:34 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#isolate the date from invalid ssh
+sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 1-15
+
 #isolate ip addr from unauthorized ssh
-#finds invalid users, cuts down the line to omit date and time information to isolate the IP address alone. then finds IP
-sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 40-| grep -E -o "[^^][0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"
+        #finds invalid users, cuts down the line to omit date and time information to isolate the IP address alone. then finds IP
+        #removes white space at beginning for IPS that don't start with 3 numbers
+sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 39-| grep -E -o "[^^][0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"|tr -d '[:blank:]'
