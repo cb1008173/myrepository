c5a705f89ea84e31f4f9c4d648cb0c84a8bf4e91
connor baun <135280720+cb1008173@users.noreply.github.com>
2023-06-29 14:33:53 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -8,6 +8,10 @@ date=$(sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 1-15)
         #removes white space at beginning for IPS that don't start with 3 numbers
 ip=$(sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 39-| grep -E -o "[^^][0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"|tr -d '[:blank:]'
 
+#isolate the country from geoiplookup
+country=$(sudo geoiplookup $ip | cut -c 24-)
+
 #echo to test that it still works with assigned variables
 echo "$date"
 echo "$ip"
+echo "$country"
