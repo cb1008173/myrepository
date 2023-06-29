7028dab1ff9acad23d8b0f535424d36ecb57a5cc
connor baun <135280720+cb1008173@users.noreply.github.com>
2023-06-29 12:36:37 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+#isolate ip addr from unauthorized ssh
+#finds invalid users, cuts down the line to omit date and time information to isolate the IP address alone. then finds IP
 sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 40-| grep -E -o "[^^][0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"
