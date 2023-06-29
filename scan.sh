ddb0a4253feeb8fa11a73e774dd49da1b3b8d4c5
connor baun <135280720+cb1008173@users.noreply.github.com>
2023-06-29 15:31:44 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -6,7 +6,7 @@ date=$(sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 1-15)
 #isolate ip addr from unauthorized ssh
         #finds invalid users, cuts down the line to omit date and time information to isolate the IP address alone. then finds IP
         #removes white space at beginning for IPS that don't start with 3 numbers
-ip=$(sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 39-| grep -E -o "[^^][0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"|tr -d '[:blank:]'
+ip=$(sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 39-| grep -E -o "[^^][0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"|tr -d '[:blank:]')
 
 #isolate the country from geoiplookup
 country=$(sudo geoiplookup $ip | cut -c 24-)
