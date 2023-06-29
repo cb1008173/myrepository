01780d0e37509102f1266b7907d2c6f2cf364d40
connor baun <135280720+cb1008173@users.noreply.github.com>
2023-06-29 13:04:58 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 #isolate the date from invalid ssh
-sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 1-15
+date=$(sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 1-15)
 
 #isolate ip addr from unauthorized ssh
         #finds invalid users, cuts down the line to omit date and time information to isolate the IP address alone. then finds IP
         #removes white space at beginning for IPS that don't start with 3 numbers
-sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 39-| grep -E -o "[^^][0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"|tr -d '[:blank:]'
+ip=$(sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 39-| grep -E -o "[^^][0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"|tr -d '[:blank:]'
+
+#echo to test that it still works with assigned variables
+echo "$date"
+echo "$ip"
