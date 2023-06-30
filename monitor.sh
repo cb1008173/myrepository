ffa56e8c43f5ccc82e942964bd11e4e7cf2dca46
connor baun <135280720+cb1008173@users.noreply.github.com>
2023-06-29 21:50:40 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -3,6 +3,8 @@ changes=$(diff /var/webserver_monitor/unauthorized.log /var/webserver_monitor/un
 
 if [ -z "$changes" ] ; then
     mail -s "changes to unauthorized.log" cb1008175@wcupa.edu <<< "no unauthorized access"
+    sudo cp /var/webserver_monitor/unauthorized.log /var/webserver_monitor/unauthorized2.log
 else
     mail -s "changes to unauthorized.log" cb1008175@wcupa.edu <<< "$changes"
+    sudo cp /var/webserver_monitor/unauthorized.log /var/webserver_monitor/unauthorized2.log
 fi
