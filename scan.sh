fdfc14af1e3a3662ba1a547ce114318e7eeb41d0
connor baun <135280720+cb1008173@users.noreply.github.com>
2023-06-29 20:23:04 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -18,4 +18,17 @@ echo "$country"
 
 
 #finally, this information needs to be APPENDED to a file called unauthorized.log which should be located, something like:
-# >> /var/webserver_log/unauthorized.log
+# >> /var/webserver_log/unauthorized.log echo "-------this separates my code------"
+
+ while IFS= read -r line
+ do
+     #echo "$line"
+     ip=$(echo "$line" | cut -c 39-| grep -E -o "[^^][0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"|tr -d '[:blank:]')
+     date=$(echo "$line" | cut -c 1-15)
+     country=$(sudo geoiplookup "$ip" | cut -c 24-)
+    if ! [ -z "$ip" ] ; then
+         echo "$ip $country $date"
+     fi
+ done <<< "$myvar"
+
+
