ab651b9b9760048defc02d1121724805c7abe6b9
connor baun <135280720+cb1008173@users.noreply.github.com>
2023-06-29 20:41:25 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-#isolate the date from invalid ssh
-date=$(sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 1-15)
+#isolate the dates from invalid ssh
+myvar=$(sudo cat /var/log/auth.log |grep "Invalid user")
+#echo "$myvar"
 
-#isolate ip addr from unauthorized ssh
+date=$(sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 1-15)
+#isolate ip addresses from unauthorized ssh
         #finds invalid users, cuts down the line to omit date and time information to isolate the IP address alone. then finds IP
         #removes white space at beginning for IPS that don't start with 3 numbers
-ip=$(sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 39-| grep -E -o "[^^][0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"|tr -d '[:blank:]')
+ip=$(sudo cat /var/log/auth.log |grep "Invalid user" | cut -c 39-| grep -E -o "[^^][0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"|tr -d '[:>
 
-#isolate the country from geoiplookup
+#isolate the countries from geoiplookup
 country=$(sudo geoiplookup $ip | cut -c 24-)
 
-#echo to test that it still works with assigned variables
-echo "$date"
-echo "$ip"
-echo "$country"
+#echo "$ip $country $date" >> /var/webserver_log/unauthorized.log
 
-
-#finally, this information needs to be APPENDED to a file called unauthorized.log which should be located, something like:
-# >> /var/webserver_log/unauthorized.log echo "
+# compare IP addresses in the auth.log file, to the IP addresses in the unauthorized.log file.
+# if there is an IP address in auth.log, that is NOT in unauthorized.log, then append.
+"$myvar" | while read -r line
+do
+     ip=$(echo "$line" | cut -c 39-| grep -E -o "[^^][0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}"|tr -d '[:blank:]')    
+     date=$(echo "$line" | cut -c 1-15)
+     echo "$ip $date"
+     if ! grep "$date" /var/webserver_log/unauthorized.log ; then
+             echo "$ip $country $date" # >> /var/webserver_log/unauthorized.log
+     fi
+done
 
  echo "-------this separates my code------"
 
